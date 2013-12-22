1953ecda53e011fe314f770bb51617f82178343f
Stefan Illgen <stefan.illgen.ebenheit@gmail.com>
2013-12-22 01:20:58 +0100

    Basic Layout designed and implemented

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,27 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.core.Router
 
+/**
+ * @author stefan.illgen
+ */
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Redirect("/me")   
+  }
+  
+  def me = Action {
+    Ok(views.html.me("Information about me .."))
+  }
+  
+  def projects = Action {
+    Ok(views.html.projects("Information about projects .."))
+  }
+  
+  def music = Action {
+    Ok(views.html.music("Information about music .."))
   }
 
 }
